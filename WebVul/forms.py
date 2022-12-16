346e42d264429924843e4a73de9c7fad87c94861
ronglai <zzrldm@gmail.com>
2022-12-16 08:37:41 +0800

    Dec-15 2022 v2

--- WebVul/forms.py
+++ WebVul/forms.py
@@ -1,18 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm
-from captcha.fields import CaptchaField, CaptchaTextInput
 from django import forms
-
-'''
-class BruteForceAuthenticationForm(AuthenticationForm):
-    captcha = CaptchaField()
-'''
-
-'''
-class UserForm(forms.Form):
-    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Username",'autofocus': ''}))
-    password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control',  'placeholder': "Password"}))
-    captcha = CaptchaField(label='验证码')
-'''
+from captcha.fields import CaptchaField, CaptchaTextInput
 
 
 class UserForm(forms.Form):
